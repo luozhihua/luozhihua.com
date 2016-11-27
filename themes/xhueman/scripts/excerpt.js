225671d8e63450ae1c55232d4f02020da42b242d
Colin Luo <mail@luozhihua.com>
2016-11-27 23:53:35 +0800

    1. add post type poem; 2. optmize description of post;

--- themes/xhueman/scripts/excerpt.js
+++ themes/xhueman/scripts/excerpt.js
@@ -4,9 +4,25 @@
 * @example
 *     <%- excerpt(post) %>
 */
+
+function split(html, maxlength) {
+	var fragments = html.split('&#');
+
+	fragments = fragments.slice(0, maxlength);
+
+	return fragments.join('&#');
+}
+
 hexo.extend.helper.register('excerpt', function (post) {
     var excerpt;
-    if (post.excerpt) {
+
+    if (post.type === 'poem') {
+    	excerpt = split(post.content
+    		.replace(/\<\/p\>/g, ' / ')
+    		.replace(/\<br[\s\/]*\>/g, ' / ')
+    		.replace(/\<[^\>]+\>/g, '')
+    		.replace(/^\s*\/\s*/, ''), 60) + '...';
+    } else if (post.excerpt) {
         excerpt = post.excerpt.replace(/\<[^\>]+\>/g, '');
     } else {
         excerpt = post.content.replace(/\<[^\>]+\>/g, '').substring(0, 200);
